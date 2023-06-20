8376f676e21ecf1872325f0bee633b2347ad07c9
Sam Lee <Sam.Lee@softwire.com>
2023-06-20 09:21:26 +0100

    implement

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,7 +3,20 @@ function fizzbuzz() {
     console.log("Hello, World!");
 
     // Put your code here...
-
+    for (let num = 1; num <= 100; num++) {
+        if (num % 15 == 0) {
+            console.log("FizzBuzz");
+        }
+        else if (num % 3 == 0) {
+            console.log("Fizz");
+        }
+        else if (num % 5 == 0) {
+            console.log("Buzz");
+        }
+        else {
+            console.log(num);
+        }
+    }
 }
 
 // Now, we run the main function:
